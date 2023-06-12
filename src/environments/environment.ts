0f465f03ccb578c59421a75f82c1eca5f3aea4f8
Monica Monteiro <monicamo@gmail.com>
2023-06-11 22:40:27 -0300

    Aula #01 - Inicio do projeto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,19 @@
-export const environment = {};
+export const environment = {
+  production: true
+};
 
 export const SpotifyConfiguration = {
-
+  clientId : '5bb43be7de21403997aca806bb3b339a',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
 }
